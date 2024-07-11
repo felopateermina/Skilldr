93d20ef0002410d6603f60dc233d8c4ae2e1d8aa
John Doe <johndoe@email.com>
2024-07-11 12:31:07 +0300

    DEBUG(BUGS)

--- api/play.code.lua.php
+++ api/play.code.lua.php
@@ -560,10 +560,7 @@ document.addEventListener('DOMContentLoaded', function () {
         const commentElement = document.createElement('div');
         commentElement.classList.add('comment');
         commentElement.innerHTML = `
-            <p><strong style="color:#de5b00;"><svg width="20" height="20" fill="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                <path fill-rule="evenodd" d="M16 9a4 4 0 1 1-8 0 4 4 0 0 1 8 0Zm-2 0a2 2 0 1 1-4 0 2 2 0 0 1 4 0Z" clip-rule="evenodd"></path>
-                <path fill-rule="evenodd" d="M12 1C5.925 1 1 5.925 1 12s4.925 11 11 11 11-4.925 11-11S18.075 1 12 1ZM3 12c0 2.09.713 4.014 1.908 5.542A8.986 8.986 0 0 1 12.065 14a8.984 8.984 0 0 1 7.092 3.458A9 9 0 1 0 3 12Zm9 9a8.963 8.963 0 0 1-5.672-2.012A6.992 6.992 0 0 1 12.065 16a6.991 6.991 0 0 1 5.689 2.92A8.964 8.964 0 0 1 12 21Z" clip-rule="evenodd"></path>
-            </svg> <span style="font-size: 20px;">${comment.username} :</span></strong></p>
+            <p><strong style="color:#de5b00;"><i class='fas fa-user'></i> <span style="font-size: 20px;">${comment.username} :</span></strong></p>
             <p>${comment.text}</p>`;
         return commentElement;
     }
