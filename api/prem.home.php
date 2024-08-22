a3ab11cd7eade29dbe087f3c8cd85de45bae84e3
John Doe <johndoe@email.com>
2024-08-23 00:01:36 +0300

    omg

--- api/prem.home.php
+++ api/prem.home.php
@@ -14,7 +14,7 @@ if (!isset($_SESSION['username'])) {
     <meta charset="UTF-8" />
     <meta name="description" content="Watch Free Online Courses With Skilldr Academy!">
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Skilldr | Free online courses </title>
+    <title>Skilldr | Premium Access </title>
     <link rel="stylesheet" href="../css/style.css" />
     <meta name="keywords" content="courses , skilldr courses , courses skilldr">
     <meta name="robots" content="index, follow">
